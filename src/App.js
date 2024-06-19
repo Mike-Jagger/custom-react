e17f40eddb2c23edcd8d1e6c331943d98c3c6810
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-06-19 13:49:45 -0600

    Adding new scss animation method

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import './App.css'; // Import the CSS file for animations
-
-const textList = [
-  "SAAS Business",
-  "Tech Startup",
-  "E-commerce Platform",
-  "Digital Agency",
-];
+import React from 'react';
+import './App.scss'; // Import the SCSS file for animations
 
 const App = () => {
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % textList.length);
-    }, 3500); // Change text every 3 seconds
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div className="container">
       <div className="text-wrapper">
         Enhance The Way You Build Your&nbsp;
         <div className="text-container">
-          <div className="text">{textList[index]}</div>
+          <div className="text-slide">
+            <div className="text">SAAS Business</div>
+            <div className="text">Tech Startup</div>
+            <div className="text">E-commerce Platform</div>
+            <div className="text">Digital Agency</div>
+          </div>
         </div>
       </div>
       <p className="description">
